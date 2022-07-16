9fdaec0ca153d92e35838304fa1c61110b3adab9
Stanislav N <stero141@gmail.com>
2022-07-17 00:58:14 +0500

    Add MainGame counter

--- app/src/main/java/ru/porcupine/whac_a_mole/Game.kt
+++ app/src/main/java/ru/porcupine/whac_a_mole/Game.kt
@@ -1,73 +1,100 @@
 package ru.porcupine.whac_a_mole
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import ru.porcupine.whac_a_mole.ui.theme.WhacAMoleTheme
+
+
+var resultCount:Int=0
+
 
 class Game : ComponentActivity() {
-
-    var time:Int = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent()
+            val clicksCount = remember{ mutableStateOf(value = resultCount)}
+            Counter(clicksCount = clicksCount)
+            MainContent(clicksCount = clicksCount)
         }
     }
 }
 
 @Composable
-fun MainContent(){
+fun MainContent(clicksCount: MutableState<Int>){
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Column {
-            Hole()
-            Hole()
-            Hole()
+            Hole(true, clicksCount = clicksCount)
+            Hole(false, clicksCount = clicksCount)
+            Hole(false,clicksCount = clicksCount)
         }
         Column {
-            Hole()
-            Hole()
-            Hole()
+            Hole(false,clicksCount = clicksCount)
+            Hole(false, clicksCount = clicksCount)
+            Hole(false, clicksCount = clicksCount)
         }
         Column {
-            Hole()
-            Hole()
-            Hole()
+            Hole(false, clicksCount = clicksCount)
+            Hole(false,clicksCount = clicksCount)
+            Hole(false,clicksCount = clicksCount)
         }
     }
 }
 
 @Composable
-fun Hole(){
+fun Hole(isMole:Boolean, clicksCount:MutableState<Int>){
+    val mole = remember { mutableStateOf(value = isMole) }
+
     IconButton(
         onClick = {
-
+            if(mole.value){
+                resultCount++
+                mole.value = false
+                clicksCount.value= resultCount
+            }
+            Log.i("Result", resultCount.toString())
         },
         modifier = Modifier.padding(15.dp)
     ) {
+        var icon: ImageVector? = null
+        icon = if(mole.value){
+            Icons.Filled.KeyboardArrowUp
+        } else{
+            Icons.Filled.KeyboardArrowDown
+        }
         Icon(
-            Icons.Filled.KeyboardArrowDown,
+            icon,
             contentDescription = "hole",
         )
+    }
+}
+
+@Composable
+fun Counter(clicksCount:MutableState<Int>){
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text= clicksCount.value.toString())
     }
 }
 
