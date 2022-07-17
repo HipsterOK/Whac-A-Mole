d9a52130232f5dc0aedf74fe1d7dc23ca4325e23
Stanislav N <stero141@gmail.com>
2022-07-17 12:58:20 +0500

    Add Timer

--- app/src/main/java/ru/porcupine/whac_a_mole/GameActivity.kt
+++ app/src/main/java/ru/porcupine/whac_a_mole/GameActivity.kt
@@ -1,5 +1,6 @@
 package ru.porcupine.whac_a_mole
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -15,47 +16,55 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 
 var resultCount:Int=0
+var seconds:Int=0
 
 
 class Game : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val coroutineScope = rememberCoroutineScope()
+            val timer = remember {  mutableStateOf(value = seconds)  }
             val clicksCount = remember{ mutableStateOf(value = resultCount)}
-            Counter(clicksCount = clicksCount)
-            MainContent(clicksCount = clicksCount)
+            val isMole = remember{ mutableStateOf(value = 1)}
+            Counter(clicksCount = clicksCount, timer = timer)
+            MainContent(clicksCount = clicksCount, molePosition = isMole)
+            LaunchedEffect(key1 = Unit) {
+                coroutineScope.launch() {
+                   while(timer.value<30){
+                       timer.value++
+                       seconds=timer.value
+                       delay(1000)
+                       val changePage = Intent(this@Game, Result::class.java)
+                       startActivity(changePage)
+                   }
+                }
+            }
         }
     }
 }
 
 @Composable
-fun MainContent(clicksCount: MutableState<Int>){
+fun MainContent(clicksCount: MutableState<Int>, molePosition: MutableState<Int>){
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column {
-            Hole(true, clicksCount = clicksCount)
-            Hole(false, clicksCount = clicksCount)
-            Hole(false,clicksCount = clicksCount)
-        }
-        Column {
-            Hole(false,clicksCount = clicksCount)
-            Hole(false, clicksCount = clicksCount)
-            Hole(false, clicksCount = clicksCount)
-        }
-        Column {
-            Hole(false, clicksCount = clicksCount)
-            Hole(false,clicksCount = clicksCount)
-            Hole(false,clicksCount = clicksCount)
+        for(i in 0..2){
+            Column() {
+                for (j in 0..2) {
+                    if(molePosition.value==(i+1)*(j+1)){
+                        Hole(true, clicksCount = clicksCount)
+                    } else Hole(false, clicksCount = clicksCount)
+                }
+            }
         }
     }
 }
@@ -89,17 +98,23 @@ fun Hole(isMole:Boolean, clicksCount:MutableState<Int>){
 }
 
 @Composable
-fun Counter(clicksCount:MutableState<Int>){
-    Column(
-        modifier = Modifier.fillMaxWidth().padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+fun Counter(clicksCount:MutableState<Int>, timer:MutableState<Int>){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(25.dp),
     ) {
-        Text(text= clicksCount.value.toString())
-    }
-}
-
-fun main() = runBlocking {
-    launch {
-        delay(1000L)
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.fillMaxWidth(0.5f)
+        ) {
+            Text(text= clicksCount.value.toString(), textAlign = TextAlign.Start)
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            modifier = Modifier.fillMaxWidth()
+            ) {
+            Text(text = timer.value.toString(), textAlign = TextAlign.End)
+        }
     }
 }
