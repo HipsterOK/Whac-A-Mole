ff32be4bd35ee137a4feb85efeba764edb20b26e
Stanislav N <stero141@gmail.com>
2022-07-17 16:53:37 +0500

    Add graphics in GameActivity

--- app/src/main/java/ru/porcupine/whac_a_mole/GameActivity.kt
+++ app/src/main/java/ru/porcupine/whac_a_mole/GameActivity.kt
@@ -1,10 +1,12 @@
 package ru.porcupine.whac_a_mole
 
 import android.content.Intent
+import android.icu.number.Scale
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -16,7 +18,11 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.*
@@ -40,6 +46,7 @@ class GameActivity : ComponentActivity() {
             val timer = remember {  mutableStateOf(value = seconds)  }
             val clicksCount = remember{ mutableStateOf(value = resultCount)}
             val isMole = remember{ mutableStateOf(value = 0)}
+            BackImage()
             Counter(clicksCount = clicksCount, timer = timer)
             MainContent(clicksCount = clicksCount, molePosition = isMole)
             LaunchedEffect(key1 = Unit) {
@@ -76,7 +83,8 @@ fun MainContent(clicksCount: MutableState<Int>, molePosition: MutableState<Int>)
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+
     ) {
         for(i in 0..2){
             Column() {
@@ -103,16 +111,22 @@ fun Hole(molePosition: MutableState<Int>, clicksCount:MutableState<Int>){
         },
         modifier = Modifier.padding(15.dp)
     ) {
-        var icon: ImageVector? = null
-        icon = if(molePosition.value==holeId.value){
-            Icons.Filled.Call
+        if(molePosition.value==holeId.value){
+            Icon(
+                modifier = Modifier.size(80.dp),
+                painter = painterResource(id = R.drawable.mole),
+                contentDescription = "mole",
+                tint = Color.Unspecified
+            )
         } else{
-            Icons.Filled.KeyboardArrowDown
+            Icon(
+                modifier = Modifier.size(80.dp),
+                painter = painterResource(id = R.drawable.hole),
+                contentDescription = "hole",
+                tint = Color.Unspecified
+            )
         }
-        Icon(
-            icon,
-            contentDescription = "hole",
-        )
+
     }
 }
 
@@ -127,13 +141,34 @@ fun Counter(clicksCount:MutableState<Int>, timer:MutableState<Int>){
             horizontalAlignment = Alignment.Start,
             modifier = Modifier.fillMaxWidth(0.5f)
         ) {
-            Text(text= clicksCount.value.toString(), textAlign = TextAlign.Start)
+            Text(
+                text= "Result: ${clicksCount.value.toString()}",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
         }
         Column(
             horizontalAlignment = Alignment.End,
             modifier = Modifier.fillMaxWidth()
             ) {
-            Text(text = timer.value.toString(), textAlign = TextAlign.End)
+            Text(
+                text = "Time: ${timer.value.toString()}",
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
         }
     }
+}
+
+@Composable
+fun BackImage() {
+    Image(
+        painter = painterResource(id = R.drawable.background),
+        contentDescription = "back",
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
 }
